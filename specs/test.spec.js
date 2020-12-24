2a728356442cc54b6d063c5eebe986e1e8609d27
Roland <rdorde@gmail.com>
2020-12-24 19:35:43 +0100

    test component html

--- specs/test.spec.js
+++ specs/test.spec.js
@@ -1,5 +1,15 @@
 import TestComponent from '@/test';
+import { mount, shallowMount } from '@vue/test-utils';
+import List from '@/list';
 
 test('first spec', () => {
-  console.log(TestComponent);
+  const wrapper = mount(TestComponent, {
+    propsData: { value: 'my value' }
+  });
+  expect(wrapper.html()).toMatchSnapshot();
+});
+
+test('ListComponent shallow', () => {
+  console.log(mount(List).html());
+  console.log(shallowMount(List).html());
 });
