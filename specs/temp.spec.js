f956e15cb8f7e60279ae8b46b109043dfbbff26b
Roland <rdorde@gmail.com>
2020-12-24 20:05:58 +0100

    test computed properties and watchers

--- specs/temp.spec.js
+++ specs/temp.spec.js
@@ -1,2 +1,35 @@
-import Temprature from '@/temprature'
-import { mount } from '@vue/test-utils'
+import Temprature from '@/temprature';
+import { mount } from '@vue/test-utils';
+
+describe('computed', () => {
+  test('celsius ', () => {
+    const { vm } = mount(Temprature);
+    expect(vm.celsius).toBe(0);
+
+    vm.degrees = 23;
+    expect(vm.celsius).toBe(23);
+  });
+
+  test('fahrenheit ', () => {
+    const { vm } = mount(Temprature);
+    expect(vm.fahrenheit).toBe(32);
+
+    vm.degrees = 16;
+    expect(vm.fahrenheit).toBe(60.8);
+  });
+});
+
+test('temp', async () => {
+  const wrapper = mount(Temprature, {
+    propsData: {
+      temp: 40
+    }
+  });
+
+  expect(wrapper.vm.degrees).toBe(40);
+  expect(wrapper.vm.type).toBe('celsius');
+
+  await wrapper.setProps({ temp: '50f' });
+  expect(wrapper.vm.degrees).toBe(50);
+  expect(wrapper.vm.type).toBe('fahrenheit');
+});
