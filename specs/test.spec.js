b9d4c8483633c0c3995baec496e1050212340834
Roland <rdorde@gmail.com>
2020-12-24 19:49:15 +0100

    get component data and set new data in the test

--- specs/test.spec.js
+++ specs/test.spec.js
@@ -2,14 +2,21 @@ import TestComponent from '@/test';
 import { mount, shallowMount } from '@vue/test-utils';
 import List from '@/list';
 
-test('first spec', () => {
+test('fmount a vue component', () => {
   const wrapper = mount(TestComponent, {
     propsData: { value: 'my value' }
   });
-  expect(wrapper.html()).toMatchSnapshot();
+  expect(wrapper).toMatchSnapshot();
 });
 
 test('ListComponent shallow', () => {
   console.log(mount(List).html());
   console.log(shallowMount(List).html());
+});
+
+test('ListComponent', async () => {
+  const wrapper = mount(List);
+  const movies = wrapper.vm.marvelMovies;
+  await wrapper.setData({ marvelMovies: [...movies, 'Endgame'] });
+  expect(wrapper).toMatchSnapshot();
 });
