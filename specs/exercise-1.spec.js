a5b2481468bba3aa91dc594a36afe3f3a29679e5
Roland <rdorde@gmail.com>
2020-12-24 23:35:31 +0100

    testing the DOM with faker data

--- specs/exercise-1.spec.js
+++ specs/exercise-1.spec.js
@@ -1,2 +1,47 @@
 import UserList from '@/exercise-1';
 import { mount } from '@vue/test-utils';
+import { name } from 'faker';
+
+function getRandomUsers(amount = 1) {
+  return Array.from({ length: amount }, () => name.findName());
+}
+
+test('component renders the user', () => {
+  const wrapper = mount(UserList, {
+    propsData: {
+      users: [name.findName()]
+    }
+  });
+
+  const li = wrapper.find('li');
+  expect(li.text()).toBe(wrapper.props('users')[0]);
+});
+
+test('component renders three user', async () => {
+  const wrapper = mount(UserList, {
+    propsData: {
+      users: getRandomUsers(3)
+    }
+  });
+  const filterInput = wrapper.find('input');
+  const listItems = wrapper.findAll('li');
+  expect(listItems.length).toBe(3);
+
+  await wrapper.vm.$nextTick(() => {
+    for (let i = 0; i < listItems.length; i++) {
+      expect(listItems.at(i).text()).toBe(wrapper.props('users')[i]);
+    }
+  });
+
+  const pickedName = listItems.at(1).text();
+  filterInput.setValue(pickedName);
+
+  await wrapper.vm.$nextTick(() => {
+    const listItems = wrapper.findAll('li');
+    expect(listItems.length).toBe(1);
+  });
+
+  await wrapper.vm.$nextTick(() => {
+    expect(listItems.at(0).text()).toBe(wrapper.props('users')[0]);
+  });
+});
