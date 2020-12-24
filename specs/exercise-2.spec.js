2969b2350807156ace2d2ceff89f35a9cf799550
Roland <rdorde@gmail.com>
2020-12-25 00:15:18 +0100

    test user flows with more complex DOM nesting with snapshots

--- specs/exercise-2.spec.js
+++ specs/exercise-2.spec.js
@@ -1,2 +1,24 @@
 import ExerciseForm from '@/exercise-2';
 import { mount } from '@vue/test-utils';
+import { nextTick } from 'vue';
+
+test('follow the user through the form', async () => {
+  const wrapper = mount(ExerciseForm);
+  expect(wrapper).toMatchSnapshot();
+
+  const form = wrapper.find('form');
+  const input = form.find('input');
+
+  input.setValue('buy bread');
+  form.trigger('submit');
+
+  await nextTick();
+  expect(wrapper).toMatchSnapshot();
+
+  const li = wrapper.find('ul').find('li');
+  const doneButton = li.find('button');
+  doneButton.trigger('click');
+
+  await nextTick();
+  expect(wrapper).toMatchSnapshot();
+});
